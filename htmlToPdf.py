5af884d527c7e72176af8bbe96b9af5858c3047e
tiankonguse <i@tiankonguse.com>
2018-11-07 02:28:14 +0800

    html转pdf，url转pdf调通

--- htmlToPdf.py
+++ htmlToPdf.py
@@ -19,7 +19,6 @@ import requests
 from bs4 import BeautifulSoup
 import sys
 
-
 style_css="""
            .radius_avatar {
                 display: inline-block;
@@ -285,27 +284,10 @@ html_template = """
 </html>
 
 """
-html_template_null = """
-{content}
 
-"""
-headers = {
-    'Cache-Control': 'max-age=0',
-    'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
-    'Cookie': 'rewardsn='
-}
-
-def readData(url):
-    """
-    网络请求,返回response对象
-    :return:
-    """
-    response = requests.get(url,headers=headers)
-    return response
-    
 def parse_html(htmlText):
     try:
+        
         result = re.search(r'var publish_time = "([^"]*)"', htmlText)
         print ("publish_time", result)
         publish_time = ""
@@ -391,65 +373,17 @@ def parse_html(htmlText):
                 attrs["src"] = attrs["data-src"]
         
         html = str(body)
-        
-        print (html)
+
         html = html_template.format(content=html, style_css=style_css)
         #html = html.encode("utf-8")
         return html
     except Exception as e:
         print("解析错误")
         print(e.what())
-        return " "
-    
-def parse_body(response, domain):
-    """
-    解析正文
-    :param response: 爬虫返回的response对象
-    :return: 返回处理后的html文本
-    """
-    try:
-        soup = BeautifulSoup(response.content, 'html.parser')
-        body = soup.find_all(id="page-content")[0]
-        #body = soup
-
-        # 加入标题, 居中显示
-        #title = soup.find('h2').get_text()
-        #center_tag = soup.new_tag("center")
-        #title_tag = soup.new_tag('h1')
-        #title_tag.string = title
-        #center_tag.insert(1, title_tag)
-        #body.insert(1, center_tag)
-
-        html = str(body)
-        # body中的img标签的src相对路径的改成绝对路径
-        pattern = "(<img .*?src=\")(.*?)(\")"
-
-        def func(m):
-            m2=m.group(2)
-            if not m2.startswith("http"):
-                print (m2)
-                rtn = "".join([m.group(1), domain, m2, m.group(3)])
-                return rtn
-            else:
-                #m2=m2.split("?")
-                #m2=m2[0]+"?tp=jpeg"
-                m2 = m2.replace("https", "http")
-                print (m2)
-                return "".join([m.group(1), m2, m.group(3)])
-        html = re.compile(pattern).sub(func, html)
-        #html = html_template_null.format(content=html)
-        html = html_template.format(content=html)
-        html = html.encode("utf-8")
-        #html = str(html)
-        #print (html)
-        return html
-    except Exception as e:
-        logging.error("解析错误", exc_info=True)
         return ""
 
-def run(url, pdfFile):
+def run(htmlFile, pdfFile):
     start = time.time()
-    domain = '{uri.scheme}://{uri.netloc}'.format(uri=urlparse(url))
     options = {
         'page-size': 'Letter',
         'margin-top': '0.75in',
@@ -467,22 +401,24 @@ def run(url, pdfFile):
         'outline-depth': 10,
     }
     htmls = []
-    response = readData(url)
-    html = parse_html(str(response.content, encoding = "utf-8"))
-    f_name = "./html/tmp.html"
-    with open(f_name, 'wb') as f:
-        f.write(bytes(html, encoding = "utf8"))
-    htmls.append(f_name)
-    pdfkit.from_file(htmls, pdfFile, options=options)
-    #for html in htmls:
-    #    os.remove(html)
+    
+    print("begin read %s" % (htmlFile))
+    with open(htmlFile, 'r') as f:
+        htmlText = f.read()
+    htmlText = parse_html(htmlText)
+    
+    print("begin write tmp.html")
+    with open("./tmp.html", 'w') as f:
+        f.write(str(htmlText))
+    
+    htmls.append("tmp.html")
+    ret = pdfkit.from_file(htmls, pdfFile, options=options)
     total_time = time.time() - start
-    print(u"总共耗时：%f 秒" % total_time)
+    print(u"ret[%d] 总共耗时：%f 秒" % (ret,total_time))
     
 
-
 def usage():
-    print ("%s url pdfFile"% (sys.argv[0]))
+    print ("%s htmlFile pdfFile"% (sys.argv[0]))
     
 if __name__ == '__main__':
     argc = len(sys.argv)
@@ -490,6 +426,6 @@ if __name__ == '__main__':
     if argc != 3:
         usage()
     else:
-        url = sys.argv[1]
+        htmlFile = sys.argv[1]
         pdfFile = sys.argv[2]
-        run(url, pdfFile)
+        run(htmlFile, pdfFile)
